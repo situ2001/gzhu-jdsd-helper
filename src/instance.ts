c616ad574eafe7deac2242a7a7409744ed8c3015
situ2001 <yongcong2001@outlook.com>
2021-09-28 11:27:16 +0800

    feat: singleton axios

---
+++ src/instance.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+// Singleton axios
+export const instance = axios.create({
+  baseURL: "https://jdsd.gzhu.edu.cn/coctl_gzhu/index_wx.php",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
+  },
+});
