e249e23b3ca5fd4aa89c43eed9ef556ce0c7c20d
Taozc <861309159@qq.com>
2021-09-30 19:25:23 +0800

    fix: 修复字段遗漏

--- src/actions/info.ts
+++ src/actions/info.ts
@@ -12,7 +12,7 @@ export default async function getInfo() {
 
   const response = await instance.post("/", form);
   if (response.data) {
-    if (response.data === 1) {
+    if (response.data.status === 1) {
       console.log("你的积分为", response.data.re.credits);
     } else {
       console.log(response.data.error);
