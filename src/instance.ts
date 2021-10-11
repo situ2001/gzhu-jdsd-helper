195920d7694e83b55233dfb454c40f531ea3dfc2
situ2001 <yongcong2001@outlook.com>
2021-10-11 22:05:35 +0800

    feat: add timeout for axios instance

--- src/instance.ts
+++ src/instance.ts
@@ -9,6 +9,7 @@ export const instance = axios.create({
     "User-Agent":
       "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 Safari/537.36 MicroMessenger/7.0.9.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat",
   },
+  timeout: 5000,
 });
 
 axiosRetry(instance, {
